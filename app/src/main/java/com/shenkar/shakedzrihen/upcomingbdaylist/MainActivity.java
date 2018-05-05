274fd4824de68b558803200a48bffdb94a64810f
shakedzrihen <shaked.zrihen@gmail.com>
2018-05-05 21:35:50 +0300

    Initial commit

---
+++ app/src/main/java/com/shenkar/shakedzrihen/upcomingbdaylist/MainActivity.java
@@ -0,0 +1,47 @@
+package com.shenkar.shakedzrihen.upcomingbdaylist;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView birthdayList;
+    RecyclerView.Adapter birthdayListAdapter;
+    ArrayList<String> names;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        birthdayList = findViewById(R.id.birthday_list);
+        birthdayList.setLayoutManager(new LinearLayoutManager(this));
+        names = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            names.add("test # " + i);
+        }
+        birthdayListAdapter = new BirthdayListAdapter(names);
+        birthdayList.setAdapter(birthdayListAdapter);
+        FloatingActionButton addNewListItem = (FloatingActionButton) findViewById(R.id.fab);
+
+
+        addNewListItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, BirthdayListManager.class));
+
+            }
+        });
+    }
+
+}
