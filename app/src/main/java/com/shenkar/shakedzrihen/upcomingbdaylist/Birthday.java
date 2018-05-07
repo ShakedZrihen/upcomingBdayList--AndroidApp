728150bce6b9f88d04b9718d072f9c44b3f9a1a6
shakedzrihen <shaked.zrihen@gmail.com>
2018-05-07 17:55:25 +0300

    Add Birthday class for handle birthday date

---
+++ app/src/main/java/com/shenkar/shakedzrihen/upcomingbdaylist/Birthday.java
@@ -0,0 +1,51 @@
+package com.shenkar.shakedzrihen.upcomingbdaylist;
+
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.icu.util.Calendar;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+
+public class Birthday {
+    private Context _context;
+    private EditText _birthday;
+    private Calendar _calendar;
+    private DatePickerDialog.OnDateSetListener _dateListener;
+
+    public Birthday(final Context context, EditText birthday) {
+        _context = context;
+        _birthday = birthday;
+        _calendar = Calendar.getInstance();
+
+        _dateListener = new DatePickerDialog.OnDateSetListener() {
+
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                _calendar.set(Calendar.YEAR, year);
+                _calendar.set(Calendar.MONTH, month);
+                _calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+
+            private void updateLabel() {
+                String myFormat = "dd/MM/yy"; //In which you need put here
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat);
+
+                _birthday.setText(sdf.format(_calendar.getTime()));
+            }
+        };
+
+        _birthday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(_context, _dateListener, _calendar
+                        .get(Calendar.YEAR), _calendar.get(Calendar.MONTH),
+                        _calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+    }
+}
