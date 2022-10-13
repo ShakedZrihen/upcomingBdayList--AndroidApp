49177c9c4136be849a31e3bd287323fdc996e7d3
shaked zohar <shaked.zrihen@gmail.com>
2022-10-13 11:51:56 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const goodbye = "bye";
 const hello = 'world';
-console.log("lololol");
+console.log("lolooolol");
 const meetAndGreet = meet => console.log('greet', meet);
 const lolipop = "lolipop";
